a1e9b6d9f84f4668be596639c6a5748d0dd9711a
dental misorder <denys.bondarenko@nure.ua>
2024-04-26 10:02:51 +0300

    Broadcast Service 📰🗞️

---
+++ Services/Discord/BroadcastDataService.cs
@@ -0,0 +1,44 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using MentallyStable.GitlabHelper.Data.Discord;
+using MentallyStable.GitlabHelper.Registrators;
+using MentallyStable.GitlabHelper.Data.Development;
+using MentallyStable.GitlabHelper.Services.Development;
+
+namespace MentallyStable.GitlabHelper.Services.Discord
+{
+    public class BroadcastDataService : IService
+    {
+        public Dictionary<ulong, BroadcastData> _broadcastData = new Dictionary<ulong, BroadcastData>();
+
+        private readonly DiscordClient _client;
+        private readonly IDebugger _debugger = new Debugger();
+
+        public const string BROADCAST_DATA_CONFIG = "discordbroadcasters.json";
+
+        public BroadcastDataService(ConfigsRegistrator configs, DiscordClient client)
+        {
+            _broadcastData = configs.BroadcastData;
+            _client = client;
+        }
+
+        public async Task InitializeService()
+        {
+            _broadcastData ??= new Dictionary<ulong, BroadcastData>();
+
+            await _debugger.TryExecuteAsync(CacheChannelsAsync(), new DebugOptions(this, nameof(CacheChannelsAsync)));
+        }
+
+        public DiscordChannel GetChannel(string channelID) => _broadcastData[ulong.Parse(channelID)].DiscodChannelReference;
+        public DiscordChannel GetChannel(ulong channelID) => _broadcastData[channelID].DiscodChannelReference;
+        public DiscordChannel GetChannel(BroadcastData data) => GetChannel(data.ChannelID);
+
+        private async Task CacheChannelsAsync()
+        {
+            foreach (var dataItem in _broadcastData.Values)
+            {
+                dataItem.DiscodChannelReference = await _client.GetChannelAsync(dataItem.ChannelID);
+            }
+        }
+    }
+}
